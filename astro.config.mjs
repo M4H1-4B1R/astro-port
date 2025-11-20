814e9a7df85b163888966d628b29ae5b9822144d
M4H1-4B1R <mahiabirgit@tutanota.com>
2025-11-20 23:13:23 +0600

    set site url and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://M4H1-4B1R.github.io',
+	base: '/astro-port',
+});
